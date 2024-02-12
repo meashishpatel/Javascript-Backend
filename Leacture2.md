08cd5736d7867131dfe013581aa7cc1d6d05ca80
meashishpatel <ashishpatel3009@gmail.com>
2024-02-12 13:10:28 +0530

    First Commit

--- Leacture2.md
+++ Leacture2.md
@@ -1,1 +1,22 @@
 ![alt text](image.png)
+
+Make a empty node application
+
+```bash
+npm init
+```
+
+## Install express using
+
+```bash
+npm install express
+```
+
+## Install dotenv using
+
+```bash
+npm i dotenv
+```
+
+> Visit
+> https://www.npmjs.com/package/dotenv
---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("meashish_patel");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1> I have created a server</h1>");
+});
+
+app.get("/celebration", (req, res) => {
+  res.send("<h1> Celebration </h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
